4bc651dc06ba2d379430435c47e096ae38ab94ab
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-06-13 16:43:38 -0300

    Tailwind with css modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import styles from "@/styles/Home.module.css"
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <h1 className={styles.title}>Hello, World!</h1>
     </main>
   )
 }
